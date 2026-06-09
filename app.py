3b8cd6172e4b0d32ef5e3251a8658808ee2058eb
Hariprasathkuppusamy <hariprasathkuppusamy2005@gmail.com>
2026-06-09 14:32:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, deployed automatically by Jenkins!"
+    return "version2 ,deployed automatically by Jenkins!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
